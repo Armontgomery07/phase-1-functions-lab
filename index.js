b55f03084f5c05681cb4041c2171e83a16e99646
Arielle Montgomery <Armontgomery07@gmail.com>
2023-04-09 14:02:41 -0400

    finally!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    const hqLocation = 42
+    return Math.abs(someValue - hqLocation)
+}
+
+function distanceFromHqInFeet(someValue) {
+  const distanceInBlocks = distanceFromHqInBlocks(someValue);
+  return distanceInBlocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(start - destination);
+    return distanceInBlocks * 264;
+  }
+
+  function calculatesFarePrice (start ,destination) {
+    const distanceInFeet = distanceTravelledInFeet(start,destination);
+    let fare;
+   if (distanceInFeet <= 400) {
+        fare = 0;
+      } 
+      else if (distanceInFeet <= 2000) {
+        fare = (distanceInFeet - 400) * 0.02;
+      }
+       else if (distanceInFeet <= 2500) {
+        fare = 25;
+      }
+       else {
+        return "cannot travel that far";
+      }
+    
+      return fare;
+    }
